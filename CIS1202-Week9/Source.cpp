80830f076386b88f6f117aaee22ce0382ca4d17c
no <no@no.no>
2021-03-18 13:15:42 -0400

    First commit

---
+++ CIS1202-Week9/Source.cpp
@@ -0,0 +1,16 @@
+// Andrew Cole
+// CIS 1202
+// Source Code Managmment
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World!" << endl;
+
+	system("pause");
+	return 0;
+}
